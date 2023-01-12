34cd62a6fe819d90d9e7aed712b922fc7f3a5927
freddy <x0freddyx0@hotmail.com>
2023-01-11 22:10:04 -0800

    removed axios

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { connect } from "./redux/blockchain/blockchainActions";
 import { fetchData } from "./redux/data/dataActions";
 import * as s from "./styles/globalStyles";
 import styled from "styled-components";
-import axios from "axios";
 
 const truncate = (input, len) =>
   input.length > len ? `${input.substring(0, len)}...` : input;
